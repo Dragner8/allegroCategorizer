66d9d7e351c4aa87136ec4a82aaa1da3fc73783d
Wojciech <wojciech.stanek@skkglobal.com>
2018-06-21 01:43:42 +0200

    working site and communication

--- Classifier/use.py
+++ Classifier/use.py
@@ -9,16 +9,23 @@ import cPickle
 from flask import Flask, render_template, request
 
 def main(url):
-    print(url)
+    listOfItems = url.split("-")
+    if 'allegro' not in listOfItems[0]:
+        return -1
+    del listOfItems[-1]
+    del listOfItems[0]
+    items = " ".join(listOfItems)
+
     with open('classifier.pkl', 'rb') as fid:
         clf = cPickle.load(fid)
 
     with open('count_vector.pkl', 'rb') as fid:
         count_vector = cPickle.load(fid)
 
-    newData = count_vector.transform(["bestia do gier", "nikon nowy zielony", "windows gtx nikon", "gtx komputer"])
+    # newData = count_vector.transform(["bestia do gier", "nikon nowy zielony", "windows gtx nikon", "gtx komputer"])
+    newData = count_vector.transform([items])
     prediction = clf.predict(newData)
-    print(prediction)
+    return prediction
 
 app = Flask(__name__)
 
@@ -28,15 +35,18 @@ def output():
     return render_template('index.html')
 
 
-@app.route('/result', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         result = request.form['username']
-        # print(result)
-        main(result)
-        # result = json.dumps(result.text, sort_keys=True, indent=4, separators=(',', ': '))
-        return render_template("index.html", result=result)
+        svmResult = main(result)
+        message = 'This is not a valid Allegro web auction.'
+        if svmResult == 1:
+            message = 'This is a computer.'
+        if svmResult == 0:
+            message = 'This is NOT a computer.s'
+        return render_template("index.html", result=message)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
